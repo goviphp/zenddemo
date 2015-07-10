98f13cf70275e466563f74c5d86e08d8c4f496a4
siva-at-php <sivanraj.g@cognizant.com>
2015-07-10 11:42:32 +0530

    Merge error resolved

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -15,18 +15,13 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-<<<<<<< HEAD
         $this->logger->info('Index/List');
-=======
 		$this->view->mgr_sales =Zend_Registry::get('mgr_sales');
->>>>>>> siva-final-branch
     }
 
     public function addAction()
     {
-<<<<<<< HEAD
         $this->logger->info('Index/Add');
-=======
 		$this->view->mgr_sales =Zend_Registry::get('mgr_sales');
 		$tmpArr =$this->view->mgr_sales;
 		if(sizeof($this->getRequest()->getPost()) > 0){
@@ -36,14 +31,11 @@ class IndexController extends Zend_Controller_Action
 			fclose($fp);
 			$this->_redirect('index');
 		}
->>>>>>> siva-final-branch
     }
 
     public function editAction()
     {
-<<<<<<< HEAD
         $this->logger->info('Index/Edit');
-=======
 		$originalVal = $this->view->mgr_sales =Zend_Registry::get('mgr_sales');
 		$this->view->ref = $this->getRequest()->getParam('ref');
 		if(sizeof($this->getRequest()->getPost()) > 0){
@@ -60,16 +52,11 @@ class IndexController extends Zend_Controller_Action
 		$this->logger->info('Index/View');
 		$this->view->mgr_sales =Zend_Registry::get('mgr_sales');
 		$this->view->ref = $this->getRequest()->getParam('ref');
->>>>>>> siva-final-branch
     }
 
     public function deleteAction()
     {
-<<<<<<< HEAD
         $this->logger->info('Index/Delete');
-    }
-}
-=======
 		$originalVal =$this->view->mgr_sales =Zend_Registry::get('mgr_sales');
 		$request = new Zend_Controller_Request_Http();
 		$this->view->ref = $this->getRequest()->getParam('ref');
@@ -91,5 +78,3 @@ class IndexController extends Zend_Controller_Action
 		return array_values($array);
 	}
 }
-
->>>>>>> siva-final-branch
